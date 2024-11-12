3772c4d394c3c828fbfae17629737ffb97a3f144
Hanka8 <hanka.maruskevicova@gmail.com>
2024-11-12 12:19:12 +0100

    Add info message to Get response

--- server/src/routes/leadRoutes.ts
+++ server/src/routes/leadRoutes.ts
@@ -6,4 +6,9 @@ const router: Router = Router();
 // POST endpoint to create a new lead
 router.post("/", createLead);
 
+// GET endpoint to check if the server is running
+router.get("/", (req, res) => {
+    res.send("Server is running, you can post to create a new lead.");
+});
+
 export default router;
